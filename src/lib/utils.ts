731994ae860b157fde231eb7d50a9a79a794e353
justin <113154738+justinlam6@users.noreply.github.com>
2026-03-20 12:33:36 -0400

    perf: reduce bundle size in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -40,3 +40,4 @@ export function capitalize(s: string): string {
 export function cn(...classes: (string | false | undefined | null)[]): string {
   return classes.filter(Boolean).join(" ");
 }
+// commit-marker-4
